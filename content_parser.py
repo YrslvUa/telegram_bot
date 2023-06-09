b7d6c36e8e28407b0bdcde9079b18bda64efd27e
Ярослав <yaroslavhoncharenko1@gmail.com>
2023-06-09 23:24:55 +0300

    added def contents parser

--- content_parser.py
+++ content_parser.py
@@ -9,16 +9,23 @@ def link_name_parser():
     response = requests.get(url)
     html = Bs(response.content, 'html.parser')
     links = html.find_all("a", href=re.compile("^/pet-care/animal-poison-control/toxic-and-non-toxic-plants/"))
-    for link in links:
-        href = link.get("href")
-        text = link.get_text().strip()
-        content_parser(url=href)
-        # print("Link:", href)
-        # print("Name:", text)
-        # print()
+    name_link_dict = {link.get_text().strip(): link.get("href") for link in links}
+    contents_parser(name_link_dict=name_link_dict)
 
-def content_parser(url):
-    pass
+
+def contents_parser(name_link_dict):
+    for link in name_link_dict.values():
+        url = f"https://www.aspca.org{link}"
+        response = requests.get(url)
+        html = Bs(response.content, 'html.parser')
+        names = html.find_all(class_="label-inline-format-label")
+        contents = html.find_all(class_="values")
+
+        names_list = [element.get_text(separator=' ').rstrip(':') for element in names]
+        contents_list = [element.get_text(separator=' ') for element in contents]
+
+        contents_dict = {name:content for name, content in zip(names_list, contents_list)}
+        print(contents_dict)
 
 
 if __name__ == "__main__":
